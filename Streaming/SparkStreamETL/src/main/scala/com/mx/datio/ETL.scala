2b57f73fdfb5a242545df4f27bf310a649314b8a
Eder <evillanuevadatio@datiobd.com>
2017-04-05 18:21:34 -0500

    tratando de generar un rdd con menos columnas y eliminando elementos dentro del rdd

--- Streaming/SparkStreamETL/src/main/scala/com/mx/datio/ETL.scala
+++ Streaming/SparkStreamETL/src/main/scala/com/mx/datio/ETL.scala
@@ -1,5 +1,7 @@
 package com.mx.datio
 
+import java.util.Date
+
 import kafka.serializer.StringDecoder
 import org.apache.spark._
 import org.apache.spark.streaming._
@@ -18,13 +20,13 @@ object ETL {
     val dsTransactions = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](  ssc, kafkaParams, topics)
 
     //val d1 = dsTransactions.flatMap(_._2.split(","))
-    val d1 = dsTransactions.flatMap( line => line._2.split(","))
+    //val d1 = dsTransactions.flatMap( line => line._2.split(","))
+    val d1 = dsTransactions.map( line => line._2.split(","))
 
 
-    //val dsTransactionsPrime = dsTransactions.flatMap( _.split(",")).filter( )
-
-
-
+    //d1.filter(  current =>  valu[4] < 0 )
+    val epoch = System.currentTimeMillis / 1000
+    d1.saveAsTextFiles("/home/evillanueva/tmp/d1"+ epoch )
 
     // Start the computation
     ssc.start()
