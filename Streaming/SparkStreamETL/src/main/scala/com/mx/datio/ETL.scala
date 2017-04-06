f8e8c52e0e50439292e5edaf14f628730b650d9e
Eder <evillanuevadatio@datiobd.com>
2017-04-06 15:33:46 -0500

    Usando simplemente createStream

--- Streaming/SparkStreamETL/src/main/scala/com/mx/datio/ETL.scala
+++ Streaming/SparkStreamETL/src/main/scala/com/mx/datio/ETL.scala
@@ -11,26 +11,34 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 object ETL {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("ETL")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(5))
 
     // hostname:port for Kafka brokers, not Zookeeper
     val kafkaParams = Map("metadata.broker.list" -> "localhost:9092")
     val topics = Set("chartrix")
 
-    val dsTransactions = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](  ssc, kafkaParams, topics)
+    //val dsTransactions = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](  ssc, kafkaParams, topics)
 
+    val kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181","spark-streaming-consumer-group", Map("chartrix" -> 5))
     //val d1 = dsTransactions.flatMap(_._2.split(","))
     //val d1 = dsTransactions.flatMap( line => line._2.split(","))
-    val d1 = dsTransactions.map( line => line._2.split(","))
+    //val d1 = dsTransactions.map( line => line._2.split(",") )
+    //val d1 = dsTransactions.filter( validateTrx )
+    kafkaStream.print()
 
 
     //d1.filter(  current =>  valu[4] < 0 )
-    val epoch = System.currentTimeMillis / 1000
-    d1.saveAsTextFiles("/home/evillanueva/tmp/d1"+ epoch )
+    //val epoch = System.currentTimeMillis / 1000
+    //d1.saveAsTextFiles("/home/evillanueva/tmp/d1"+ epoch )
 
     // Start the computation
     ssc.start()
     ssc.awaitTermination()
 
   }
+  /*
+  def validateTrx( d1: String ): Boolean{
+    false
+  }
+  */
 }
