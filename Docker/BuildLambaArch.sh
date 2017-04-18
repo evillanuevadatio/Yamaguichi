c4e1b627b796c8a8ddbc6f19b7710999e4e0955a
Eder <evillanuevadatio@datiobd.com>
2017-04-18 13:00:36 -0500

    Agregando dockersfile base para generar infra lamda, solo cascaron

--- Docker/BuildLambaArch.sh
+++ Docker/BuildLambaArch.sh
@@ -5,7 +5,8 @@ sudo docker rmi NodeBase
 sudo docker rmi hadoop-base
 
 # Create Network for hadoop
-#sudo docker network create --driver=host --subnet=172.10.2.0/30 --gateway=172.10.0.1 --ip-range=172.10.0.0/50 nethadoop
+#sudo docker network create --driver=host \ 
+#    --subnet=172.10.2.0/30 --gateway=172.10.0.1 --ip-range=172.10.0.0/50 nethadoop
 
 # Create Image
 sudo docker build -t node-base ./NodeBase/
